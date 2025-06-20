48a7942ffb4f85e03511da8113d9796ca005abf6
HenriqueSouza1112 <henrique.verginelli.souza@escola.pr.gov.br>
2025-06-20 01:33:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+  'Comunicação',
+  'Qual é o principal objetivo da comunicação no marketing?',
+  'Transmitir a mensagem certa ao público certo no momento certo'
+)
+
+criaCartao(
+  'Vendas',
+  'O que é funil de vendas?',
+  'Um modelo que representa as etapas que um cliente percorre até realizar uma compra'
+)
+
+criaCartao(
+  'Audiovisual',
+  'Por que o conteúdo em vídeo é tão eficaz no marketing?',
+  'Porque é dinâmico, atrativo e gera maior engajamento com o público'
+)
+
+criaCartao(
+  'Branding',
+  'O que significa o termo “branding”?',
+  'É o processo de construção e gestão da marca'
+)
